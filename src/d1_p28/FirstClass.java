bc32347b44fef51f45f621b824dca04f14b7af5a
sangryul <ksr9709@gmail.com>
2026-06-11 10:49:47 +0900

    Initial commit

---
+++ src/d1_p28/FirstClass.java
@@ -0,0 +1,16 @@
+package d1_p28;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+    //    System.out.println("이 문장이 실행될까요?......1");
+
+        System.out.println("이 문장이 실행될까요?......2");
+
+    //    System.out.println("이 문장이 실행될까요?......3");
+    //    System.out.println("이 문장이 실행될까요?......4");
+
+
+    }
+}
